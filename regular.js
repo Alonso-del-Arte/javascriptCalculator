73f7b5293ed0e6047eef321dee24c84a6841b8cb
octavius <mooreoctavius94@gmail.com>
2018-01-25 20:40:16 -0500

    finishing javascript

--- regular.js
+++ regular.js
@@ -5,20 +5,20 @@ var totalString;
 var nine = $(".nine").click(function(){
 console.log('9');
 memory.push(9);
-$(".display").html('9');
+$(".display").append('9');
 });
 
 var eight = $(".eight").click(function(){
 console.log('8');
 memory.push(8);
-$(".display").html('8');
+$(".display").append('8');
 
 });
 
 var seven = $(".seven").click(function(){
 console.log('7');
 memory.push(7);
-$(".display").html('7');
+$(".display").append('7');
 
 });
 
@@ -26,7 +26,7 @@ $(".display").html('7');
 var six = $(".six").click(function(){
 console.log('6');
 memory.push(6);
-$(".display").html('6');
+$(".display").append('6');
 
 });
 
@@ -34,42 +34,42 @@ $(".display").html('6');
 var five = $(".five").click(function(){
 console.log('5');
 memory.push(5);
-$(".display").html('5');
+$(".display").append('5');
 
 });
 
 var four = $(".four").click(function(){
 console.log('4');
 memory.push(4);
-$(".display").html('4');
+$(".display").append('4');
 
 });
 
 var four = $(".three").click(function(){
 console.log('3');
 memory.push(3);
-$(".display").html('3');
+$(".display").append('3');
 
 });
 
 var two = $(".two").click(function(){
 console.log('2');
 memory.push(2);
-$(".display").html('2');
+$(".display").append('2');
 
 });
 
 var one = $(".one").click(function(){
 console.log('1');
 memory.push(1);
-$(".display").html('1');
+$(".display").append('1');
 
 });
 
 var zero = $(".zero").click(function(){
 console.log('0');
 memory.push(0);
-$(".display").html('0');
+$(".display").append('0');
 
 });
 
@@ -81,14 +81,14 @@ $(".display").html('0');
 
 var clear = $(".clear").click(function(){
 console.log(memory = []);
-return memory = [];
 $(".display").html('');
+return memory = [];
 });
 
 var divide = $(".divide").click(function(){
 console.log('/');
 memory.push('/');
-$(".display").html('÷');
+$(".display").append('÷');
 
 });
 
@@ -100,20 +100,20 @@ $(".display").html('÷');
 var multipy = $(".minus").click(function(){
 console.log('-');
 memory.push('-');
-$(".display").html('-');
+$(".display").append('-');
 
 });
 
 var multipy = $(".plus").click(function(){
 console.log('+');
 memory.push('+');
-$(".display").html('+');
+$(".display").append('+');
 
 });
 
 var equals = $(".equals").click(function(){
 totalString = memory.join('');
 console.log(eval(totalString));
-$(".display").append(eval(totalString));
+$(".display").text(eval(totalString));
 });
 });
